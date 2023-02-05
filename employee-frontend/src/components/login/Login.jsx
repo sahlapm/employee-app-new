5a1242ad38ad66eb76f1e4686a19a67684f5d32f
Sahla <sahlapm@gmail.om>
2023-02-05 19:49:59 +0300

    commit

--- employee-frontend/src/components/login/Login.jsx
+++ employee-frontend/src/components/login/Login.jsx
@@ -26,7 +26,7 @@ const userAuthentication = ()=>{
         "username":username,
         "password":password
       }
-      axios.post(`/api/user/signin`,
+      axios.post(`http://localhost:8082/api/user/signin`,
       userData
       ).then((getData)=>{
 
